69aa26b171ad240ab906c96557c81969417ee4d9
darien bryant <darienbryant11@gmail.com>
2020-11-28 21:45:53 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -9,9 +9,9 @@ def fourth_challenge
           :age => "17",
           :attitude => "worried"}
           
-          {:name => "Mercutio",
-          :age => "18",
-          :attitude => "hot-headed"}
+          {name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
