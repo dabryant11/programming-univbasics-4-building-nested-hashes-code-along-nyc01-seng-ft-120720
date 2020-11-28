7ab6f8b7e650a7a98df765efe47dae2dfbde66c5
darien bryant <darienbryant11@gmail.com>
2020-11-28 21:29:16 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,13 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach =>
-      :matriach =>
-      :hero =>
+      :patriach => {},
+      :matriach => {},
+      :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriach => {},
+      :matriach => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
   
